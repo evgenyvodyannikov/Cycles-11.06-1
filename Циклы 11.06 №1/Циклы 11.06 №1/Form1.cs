00c17c861f0275921668841e1d3413af1c9283ed
EvgenyVodyannikov <vodyannikov.eugene@gmail.com>
2020-06-11 19:06:52 +0500

    не работает х3

--- Циклы 11.06 №1/Циклы 11.06 №1/Form1.cs
+++ Циклы 11.06 №1/Циклы 11.06 №1/Form1.cs
@@ -16,23 +16,35 @@ namespace Циклы_11._06__1
         {
             InitializeComponent();
         }
+        static int Factorial(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
 
-		private void button1_Click(object sender, EventArgs e)
-		{
+        private void button1_Click(object sender, EventArgs e)
+        {
             double.TryParse(textBox1.Text, out double x);
             double.TryParse(textBox2.Text, out double E);
-            double right = 1, u = 1, dife;
+            double right = 1, element = 1;
             int n = 0;
             do
             {
-                right *= u;
                 if (n != 1)
                 {
-                    u *= 1 - x * x / (Math.Pow(n - 1, 2) * Math.PI * Math.PI);
+                    right *= element;
+                    //element = 1 - x * x / (Math.Pow(n - 1, 2) * Math.PI * Math.PI);
+                    element = Math.Pow(-1, n) * Math.Pow(x, 2 * n + 1) / Factorial(2*n + 1);
                 }
                 n++;
             }
-            while (E <= Math.Abs(Math.Sin(x) - right));
+            while (E <= Math.Abs(Math.Sin(x) - right) || n <= 150);
             MessageBox.Show("" + Math.Sin(x).ToString("f4") + "  " + right.ToString("f4") + " " + n);
         }
 	}
