3303c2f856dc02bfb278197280367cb4356d33a9
EvgenyVodyannikov <vodyannikov.eugene@gmail.com>
2020-06-11 16:11:58 +0500

    не работает х2

--- Циклы 11.06 №1/Циклы 11.06 №1/Form1.cs
+++ Циклы 11.06 №1/Циклы 11.06 №1/Form1.cs
@@ -23,16 +23,16 @@ namespace Циклы_11._06__1
             double.TryParse(textBox2.Text, out double E);
             double right = 1, u = 1, dife;
             int n = 0;
-            while (E <= Math.Abs(Math.Sin(x) - right))
+            do
             {
+                right *= u;
                 if (n != 1)
                 {
-                    right *= u;
-                    u = 1 - x * x / Math.Pow(n - 1, 2) * Math.PI * Math.PI;
+                    u *= 1 - x * x / (Math.Pow(n - 1, 2) * Math.PI * Math.PI);
                 }
                 n++;
             }
-            
+            while (E <= Math.Abs(Math.Sin(x) - right));
             MessageBox.Show("" + Math.Sin(x).ToString("f4") + "  " + right.ToString("f4") + " " + n);
         }
 	}
