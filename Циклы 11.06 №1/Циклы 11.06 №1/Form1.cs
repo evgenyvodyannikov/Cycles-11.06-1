d60775fa88c1001db2e7e2f70481d4ab1a14ff48
EvgenyVodyannikov <vodyannikov.eugene@gmail.com>
2020-06-11 14:26:53 +0500

    хмм

--- Циклы 11.06 №1/Циклы 11.06 №1/Form1.cs
+++ Циклы 11.06 №1/Циклы 11.06 №1/Form1.cs
@@ -16,5 +16,23 @@ namespace Циклы_11._06__1
         {
             InitializeComponent();
         }
-    }
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+            double.TryParse(textBox1.Text, out double x);
+            double.TryParse(textBox2.Text, out double E);
+            double s = Math.Sin(x);
+            double right = 0, u = 1, dife;
+            int n = 1;
+            do
+            {
+                n++;
+                right += u;
+                u *= 1 - x * x / Math.Pow(n - 1, 2) * Math.PI * Math.PI;
+                dife = s - right;
+            }
+            while (Math.Abs(dife) > E);
+            MessageBox.Show("" + s + "  " + right + " " + n);
+        }
+	}
 }
